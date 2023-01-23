1d0e73fe9c7323a6eca22f9032a42a191e05174c
MLamagna <marcus@mlamagna.com.br>
2023-01-23 01:36:57 -0300

    Atualizar no github
    Atualizar no github

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+//@dart=2.9
+import 'package:buscador_gifs/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'package:buscador_gifs/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  //Definindo tema para o app
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.white),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          hintStyle: TextStyle(color: Colors.white),
+        )),
+  ));
+}
+
